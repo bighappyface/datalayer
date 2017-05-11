577359ce59eecb3edc1cdd59b46e943e4dcffff5
munavijayalakshmi <munavijayalakshmi@3508025.no-reply.drupal.org>
2017-05-10 19:25:29 -0500

    Issue #2871683 by Munavijayalakshmi, bighappyface: $this->t() should be used instead of t() for Drupal 8 version

--- src/Form/DatalayerSettingsForm.php
+++ src/Form/DatalayerSettingsForm.php
@@ -104,11 +104,11 @@ class DatalayerSettingsForm extends ConfigFormBase {
 
     $form['global'] = [
       '#type' => 'fieldset',
-      '#title' => t('Global'),
+      '#title' => $this->t('Global'),
     ];
     $form['global']['add_page_meta'] = [
       '#type' => 'checkbox',
-      '#title' => t('Add entity meta data to pages'),
+      '#title' => $this->t('Add entity meta data to pages'),
       '#default_value' => $datalayer_settings->get('add_page_meta'),
     ];
     $form['global']['output_terms'] = [
@@ -120,36 +120,36 @@ class DatalayerSettingsForm extends ConfigFormBase {
             ]
           ]
         ],
-      '#title' => t('Include taxonomy terms'),
+      '#title' => $this->t('Include taxonomy terms'),
       '#default_value' => $datalayer_settings->get('output_terms'),
     ];
     $form['global']['output_fields'] = [
       '#type' => 'checkbox',
-      '#description' => t('Exposes a checkbox on field settings forms to expose data.'),
-      '#title' => t('Include enabled field values'),
+      '#description' => $this->t('Exposes a checkbox on field settings forms to expose data.'),
+      '#title' => $this->t('Include enabled field values'),
       '#default_value' => $datalayer_settings->get('output_fields'),
     ];
     $form['global']['lib_helper'] = [
       '#type' => 'checkbox',
-      '#title' => t('Include "data layer helper" library'),
+      '#title' => $this->t('Include "data layer helper" library'),
       '#default_value' => $datalayer_settings->get('lib_helper'),
-      '#description' => t('Provides the ability to process messages passed to the dataLayer. See: <a href=":helper">data-layer-helper</a> on GitHub.', [
+      '#description' => $this->t('Provides the ability to process messages passed to the dataLayer. See: <a href=":helper">data-layer-helper</a> on GitHub.', [
         ':helper' => 'https://github.com/google/data-layer-helper'
       ]),
     ];
     if (\Drupal::moduleHandler()->moduleExists('group')) {
       $form['global']['group'] = [
         '#type' => 'checkbox',
-        '#title' => t('Group module support'),
+        '#title' => $this->t('Group module support'),
         '#default_value' => $datalayer_settings->get('group'),
-        '#description' => t('Output the group entities on pages beloging to a group.'),
+        '#description' => $this->t('Output the group entities on pages beloging to a group.'),
       ];
     }
 
     $form['entity_meta'] = [
       '#type' => 'details',
-      '#title' => t('Entity meta data'),
-      '#description' => t('The meta data details to ouput for client-side consumption. Marking none will output everything available.'),
+      '#title' => $this->t('Entity meta data'),
+      '#description' => $this->t('The meta data details to ouput for client-side consumption. Marking none will output everything available.'),
     ];
     $form['entity_meta']['global_entity_meta'] = [
       '#type' => 'checkboxes',
@@ -167,45 +167,45 @@ class DatalayerSettingsForm extends ConfigFormBase {
 
     $form['ia'] = [
       '#type' => 'details',
-      '#title' => t('Path architecture'),
-      '#description' => t('Settings for output of url path components.'),
+      '#title' => $this->t('Path architecture'),
+      '#description' => $this->t('Settings for output of url path components.'),
     ];
 
     $form['ia']['enable_ia'] = [
       '#type' => 'checkbox',
-      '#title' => t('Enable IA'),
+      '#title' => $this->t('Enable IA'),
       '#default_value' => $datalayer_settings->get('enable_ia'),
-      '#description' => t('Output url path components as datalayer attributes.'),
+      '#description' => $this->t('Output url path components as datalayer attributes.'),
     ];
 
     $ia_depth = $datalayer_settings->get('ia_depth');
     $form['ia']['ia_depth'] = [
       '#type' => 'number',
-      '#title' => t('Depth of paths'),
+      '#title' => $this->t('Depth of paths'),
       '#default_value' => isset($ia_depth) ? $ia_depth : '3',
-      '#description' => t('Define how many url path components get output in dataLayer.'),
+      '#description' => $this->t('Define how many url path components get output in dataLayer.'),
     ];
 
     $ia_cat_primary = $datalayer_settings->get('ia_category_primary');
     $form['ia']['ia_category_primary'] = [
       '#type' => 'textfield',
-      '#title' => t('Depth of paths'),
+      '#title' => $this->t('Depth of paths'),
       '#default_value' => isset($ia_cat_primary) ? $ia_cat_primary : 'primaryCategory',
-      '#description' => t('Define the key for the primary path component.'),
+      '#description' => $this->t('Define the key for the primary path component.'),
     ];
 
     $iacatSub = $datalayer_settings->get('ia_category_sub');
     $form['ia']['ia_category_sub'] = [
       '#type' => 'textfield',
-      '#title' => t('Depth of paths'),
+      '#title' => $this->t('Depth of paths'),
       '#default_value' => isset($iacatSub) ? $iacatSub : 'subCategory',
-      '#description' => t('Define the key for sub-components (this value will get appended with numerical identifier).'),
+      '#description' => $this->t('Define the key for sub-components (this value will get appended with numerical identifier).'),
     ];
 
     $form['vocabs'] = [
       '#type' => 'details',
-      '#title' => t('Taxonomy'),
-      '#description' => t('The vocabularies which should be output within page meta data. Marking none will output everything available.'),
+      '#title' => $this->t('Taxonomy'),
+      '#description' => $this->t('The vocabularies which should be output within page meta data. Marking none will output everything available.'),
     ];
     $form['vocabs']['vocabs'] = [
       '#type' => 'checkboxes',
@@ -223,15 +223,15 @@ class DatalayerSettingsForm extends ConfigFormBase {
 
     $form['user'] = [
       '#type' => 'details',
-      '#title' => t('User Details'),
-      '#description' => t('Details about the current user can be output to the dataLayer.'),
+      '#title' => $this->t('User Details'),
+      '#description' => $this->t('Details about the current user can be output to the dataLayer.'),
     ];
 
     $form['user']['expose_user_details'] = [
       '#type' => 'textarea',
-      '#title' => t('Expose user details'),
+      '#title' => $this->t('Expose user details'),
       '#default_value' => $datalayer_settings->get('expose_user_details'),
-      '#description' => t('Pages that should expose active user details to the dataLayer. Leaving empty will expose nothing.'),
+      '#description' => $this->t('Pages that should expose active user details to the dataLayer. Leaving empty will expose nothing.'),
     ];
 
     $user_roles =  user_roles(TRUE);
@@ -243,75 +243,75 @@ class DatalayerSettingsForm extends ConfigFormBase {
       '#type' => 'checkboxes',
       '#options' => $role_options,
       '#multiple' => TRUE,
-      '#title' => t('Expose user roles'),
+      '#title' => $this->t('Expose user roles'),
       '#default_value' => $datalayer_settings->get('expose_user_details_roles'),
-      '#description' => t('Roles that should expose active user details to the dataLayer. Leaving empty will expose to all roles.'),
+      '#description' => $this->t('Roles that should expose active user details to the dataLayer. Leaving empty will expose to all roles.'),
     ];
 
     $form['user']['expose_user_details_fields'] = [
       '#type' => 'checkbox',
-      '#title' => t('Include enabled user field values'),
+      '#title' => $this->t('Include enabled user field values'),
       '#default_value' => $datalayer_settings->get('expose_user_details_fields'),
     ];
 
     $form['output'] = [
       '#type' => 'details',
-      '#title' => t('Data layer output keys'),
-      '#description' => t('Define keys used in the datalayer output. Keys for field values are configurable via the field edit form.'),
+      '#title' => $this->t('Data layer output keys'),
+      '#description' => $this->t('Define keys used in the datalayer output. Keys for field values are configurable via the field edit form.'),
     ];
 
     // Entity title
     $entity_title = $datalayer_settings->get('entity_title');
     $form['output']['entity_title'] = [
       '#type' => 'textfield',
-      '#title' => t('Entity title'),
+      '#title' => $this->t('Entity title'),
       '#default_value' => isset($entity_title) ? $entity_title : 'entityTitle',
-      '#description' => t('Key for the title of an entity, e.g. node title, taxonomy term name, or username.'),
+      '#description' => $this->t('Key for the title of an entity, e.g. node title, taxonomy term name, or username.'),
     ];
 
     // Entity type.
     $entity_type = $datalayer_settings->get('entity_type');
     $form['output']['entity_type'] = [
       '#type' => 'textfield',
-      '#title' => t('Entity type'),
+      '#title' => $this->t('Entity type'),
       '#default_value' => isset($entity_type) ? $entity_type : 'entityType',
-      '#description' => t('Key for the type of an entity, e.g. node, user, or taxonomy_term.'),
+      '#description' => $this->t('Key for the type of an entity, e.g. node, user, or taxonomy_term.'),
     ];
 
     // Entity bundle.
     $entity_bundle = $datalayer_settings->get('entity_bundle');
     $form['output']['entity_bundle'] = [
       '#type' => 'textfield',
-      '#title' => t('Entity bundle'),
+      '#title' => $this->t('Entity bundle'),
       '#default_value' => isset($entity_bundle) ? $entity_bundle : 'entityBundle',
-      '#description' => t('Key for the bundle of an entity, e.g. page, my_things.'),
+      '#description' => $this->t('Key for the bundle of an entity, e.g. page, my_things.'),
     ];
 
     // Entity indetifier.
     $entity_id = $datalayer_settings->get('entity_identifier');
     $form['output']['entity_identifier'] = [
       '#type' => 'textfield',
-      '#title' => t('Entity identifier'),
+      '#title' => $this->t('Entity identifier'),
       '#default_value' => isset($entity_id) ? $entity_id : 'entityIdentifier',
-      '#description' => t('Key for the identifier of an entity, e.g. nid, uid, or tid.'),
+      '#description' => $this->t('Key for the identifier of an entity, e.g. nid, uid, or tid.'),
     ];
 
     // drupalLanguage.
     $drupal_lang = $datalayer_settings->get('drupal_language');
     $form['output']['drupal_language'] = [
       '#type' => 'textfield',
-      '#title' => t('Drupal language'),
+      '#title' => $this->t('Drupal language'),
       '#default_value' => isset($drupal_lang) ? $drupal_lang : 'drupalLanguage',
-      '#description' => t('Key for the language of the site.'),
+      '#description' => $this->t('Key for the language of the site.'),
     ];
 
     // drupalCountry.
     $drupal_country = $datalayer_settings->get('drupal_country');
     $form['output']['drupal_country'] = [
       '#type' => 'textfield',
-      '#title' => t('Drupal country'),
+      '#title' => $this->t('Drupal country'),
       '#default_value' => isset($drupal_country) ? $drupal_country : 'drupalCountry',
-      '#description' => t('Key for the country of the site.'),
+      '#description' => $this->t('Key for the country of the site.'),
     ];
 
     if (\Drupal::moduleHandler()->moduleExists('group')) {
@@ -319,9 +319,9 @@ class DatalayerSettingsForm extends ConfigFormBase {
       $group_key = $datalayer_settings->get('group_key');
       $form['output']['group_key'] = [
         '#type' => 'textfield',
-        '#title' => t('Group key'),
+        '#title' => $this->t('Group key'),
         '#default_value' => isset($group_key) ? $group_key : 'groupKey',
-        '#description' => t('Key for the group.'),
+        '#description' => $this->t('Key for the group.'),
       ];
     }
 
@@ -329,18 +329,18 @@ class DatalayerSettingsForm extends ConfigFormBase {
     $drupal_sitename = $datalayer_settings->get('site_name');
     $form['output']['site_name'] = [
       '#type' => 'textfield',
-      '#title' => t('Drupal site name'),
+      '#title' => $this->t('Drupal site name'),
       '#default_value' => isset($drupal_sitename) ? $drupal_sitename : 'drupalSitename',
-      '#description' => t('Key for the site name value.'),
+      '#description' => $this->t('Key for the site name value.'),
     ];
 
     // find an replace.
     $key_replacements = $datalayer_settings->get('key_replacements');
     $form['output']['key_replacements'] = [
       '#type' => 'textarea',
-      '#title' => t('Exposed field sub-key replacements'),
+      '#title' => $this->t('Exposed field sub-key replacements'),
       '#default_value' => !empty($key_replacements) ? $this->keyReplacementsFromArray($key_replacements) : '',
-      '#description' => t('For exposed fields with a sub-array of field data, enter a replacement key using the format: returned_value|replacement'),
+      '#description' => $this->t('For exposed fields with a sub-array of field data, enter a replacement key using the format: returned_value|replacement'),
     ];
 
     return parent::buildForm($form, $form_state);
